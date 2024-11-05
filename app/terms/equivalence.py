d88a4e1559da02d05b0485ac2ab65349001315f9
Fel1-of <fivehellday@gmail.com>
2024-11-05 22:27:51 +0300

    add Equal and test

--- app/terms/equivalence.py
+++ app/terms/equivalence.py
@@ -1,5 +1,37 @@
 from .abstract import BinaryOperator
+from .implication import Arrow
+from .negation import Not
+from app.utils import is_var_or_unary_operator
 
 
 class Equal(BinaryOperator):
     _symbol = '='
+
+    def humanize(self) -> str:
+        if is_var_or_unary_operator(self.arg1):
+            left = str(self.arg1.humanize())
+        else:
+            left = f'({self.arg1.humanize()})'
+
+        if is_var_or_unary_operator(self.arg2):
+            right = str(self.arg2.humanize())
+        else:
+            right = f'({self.arg2.humanize()})'
+        return f'{left} эквивалентно {right}'
+
+    def to_implication_view(self):
+        return Not(
+            Arrow(
+                Arrow(
+                    self.arg1.to_implication_view(),
+                    self.arg2.to_implication_view()
+                ),
+                Not(
+                    Arrow(
+                        self.arg2.to_implication_view(),
+                        self.arg1.to_implication_view()
+                    )
+                )
+            )
+        )
+
---
+++ tests/terms/equivalence_test.py
@@ -0,0 +1,18 @@
+from app.terms import Equal, Var
+
+
+def test_str():
+    assert str(Equal(Var('A'), Var('B'))) == 'A = B'
+
+
+def test_humanize():
+    assert Equal(Var('A'), Var('B')).humanize() == 'A эквивалентно B'
+
+
+def test_complex_humanize():
+    ar = Equal(Equal(Var('A'), Var('B')), Equal(Var('C'), Var('D')))
+    assert ar.humanize() == '(A эквивалентно B) эквивалентно (C эквивалентно D)'
+
+
+def test_to_implication_view():
+    assert str(Equal(Var('A'), Var('B')).to_implication_view()) == '!((A > B) > !(B > A))'
