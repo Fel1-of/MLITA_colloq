2cfc86a47a5e6a0aff5efa41cc48ca2bda38b846
Iwan Ryžij <teum3048@yandex.ru>
2024-11-07 01:23:44 +0300

    Fix #9 Incorrect humanize in OR and add some tests

--- tests/terms/exclusive_disjunction_test.py
+++ tests/terms/exclusive_disjunction_test.py
@@ -1,8 +1,12 @@
-from app.terms import Xor, Var
+from app.terms import Xor, Var, Not
 
 
 def test_str():
     assert str(Xor(Var('A'), Var('B'))) == 'A + B'
+
+
+def test_str_with_not():
+    assert str(Xor(Var('A'), Not(Var('B')))) == 'A + !B'
 
 
 def test_humanize():
@@ -11,10 +15,16 @@ def test_humanize():
 
 def test_complex_humanize():
     ar = Xor(Xor(Var('A'), Var('B')), Xor(Var('C'), Var('D')))
-    assert ar.humanize() \
-           == 'либо (либо A, либо B), либо (либо C, либо D)'
+    assert ar.humanize() == 'либо (либо A, либо B), либо (либо C, либо D)'
+
+
+def test_not_humanize():
+    ar = Xor(Not(Var('A')), Var('B'))
+    assert ar.humanize() == 'либо не A, либо B'
 
 
 def test_to_implication_view():
-    assert str(Xor(Var('A'), Var('B')).to_implication_view()) \
-           == '!((!A > B) > !(A > !B))'
+    assert (
+        str(Xor(Var('A'), Var('B')).to_implication_view())
+        == '!((!A > B) > !(A > !B))'
+    )
--- tests/terms/implication_test.py
+++ tests/terms/implication_test.py
@@ -1,12 +1,21 @@
-from app.terms import Arrow, Var
+from app.terms import Arrow, Var, Not
 
 
 def test_str():
     assert str(Arrow(Var('A'), Var('B'))) == 'A > B'
 
 
+def test_str_with_not():
+    assert str(Arrow(Var('A'), Not(Var('B')))) == 'A > !B'
+
+
 def test_humanize():
     assert Arrow(Var('A'), Var('B')).humanize() == 'если A, то B'
+
+
+def test_not_humanize():
+    ar = Arrow(Not(Var('A')), Var('B'))
+    assert ar.humanize() == 'если не A, то B'
 
 
 def test_complex_humanize():
