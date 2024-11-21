0305edfda1f2dc12a261f5d03dafe86841318e0e
Fel1-of <fivehellday@gmail.com>
2024-11-22 01:43:03 +0300

    Add hypothetical syllogism with tests

---
+++ app/utils/hypothetical_syllogism.py
@@ -0,0 +1,47 @@
+from string import ascii_lowercase, ascii_uppercase
+
+from app.terms.implication import Arrow
+from .syllogism_result import SyllogismResult
+
+SYLLOGISM_NAME = 'hypothetical syllogism'
+
+
+def hypothetical_syllogism(implication_1: Arrow, implication_2: Arrow) -> list[SyllogismResult]:
+    if not isinstance(implication_1, Arrow) or not isinstance(implication_2, Arrow):
+        return []
+    impl_1 = implication_1.unify(ascii_uppercase)
+    impl_2 = implication_2.unify(ascii_uppercase)
+    twin_impl_1 = implication_1.unify(ascii_lowercase)
+    twin_impl_2 = implication_2.unify(ascii_lowercase)
+
+    sub_map_2_to_1 = impl_2.arg1.get_substitution_map(twin_impl_1.arg2)
+    sub_map_1_to_2 = impl_1.arg2.get_substitution_map(twin_impl_2.arg1)
+
+    res = []
+    if sub_map_2_to_1 is not None:
+        output_term = Arrow(twin_impl_1.arg1, impl_2.arg2.substitute(**sub_map_2_to_1))
+        output_term = output_term.unify()
+
+        syllogism_result = SyllogismResult(
+            syllogism_name=SYLLOGISM_NAME,
+            input_terms=[implication_1, implication_2],
+            substitutions=[{}, sub_map_2_to_1],
+            output_term=output_term,
+        )
+
+        res.append(syllogism_result)
+
+    if sub_map_1_to_2 is not None:
+        output_term = Arrow(impl_1.arg1.substitute(**sub_map_1_to_2), twin_impl_2.arg2)
+        output_term = output_term.unify()
+
+        syllogism_result = SyllogismResult(
+            syllogism_name=SYLLOGISM_NAME,
+            input_terms=[implication_1, implication_2],
+            substitutions=[{}, sub_map_1_to_2],
+            output_term=output_term,
+        )
+
+        res.append(syllogism_result)
+
+    return res
---
+++ tests/utils/hypothetical_syllogism_test.py
@@ -0,0 +1,38 @@
+from app.terms.conjunction import And
+from app.terms.implication import Arrow
+from app.terms.negation import Not
+from app.terms.variable import Var
+from app.utils.hypothetical_syllogism import hypothetical_syllogism
+
+
+def test_hypothetical_syllogism_from_task():
+    A = Var('A')
+    B = Var('B')
+    implication_1 = Arrow(A, B)
+    C = Var('C')
+    implication_2 = Arrow(B, C)
+    outputs = hypothetical_syllogism(implication_1, implication_2)
+    assert len(outputs) == 2
+    assert outputs[0].output_term == Arrow(A, C)
+    assert outputs[1].output_term == Arrow(A, C)
+
+
+def test_hypothetical_syllogism_not_funny():
+    A = Var('A')
+    B = Var('B')
+    implication_1 = Arrow(Not(A), Not(A))
+    implication_2 = Arrow(A, Arrow(B, A))
+
+    outputs = hypothetical_syllogism(implication_1, implication_2)
+    assert len(outputs) == 1
+    assert outputs[0].output_term == Arrow(Not(Var('A')), Arrow(Var('B'), Not(Var('A'))))
+
+
+def test_hypothetical_syllogism_with_axioms():
+    A = Var('A')
+    B = Var('B')
+    impl1 = Arrow(And(A, B), B)
+    impl2 = Arrow(Not(A), Arrow(A, B))
+    outputs = hypothetical_syllogism(impl1, impl2)
+    assert len(outputs) == 1
+    assert outputs[0].output_term == Arrow(And(Var('A'), Not(Var('B'))), Arrow(Var('B'), Var('C')))
