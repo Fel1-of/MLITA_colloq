80e80168996ddd5829dcaff4669d8b7fb6f07fef
Iwan Ryžij <teum3048@yandex.ru>
2024-11-22 04:20:23 +0300

    Add pretty str
    Что-то вроде этого вышло
    ```
    0. axiom:       (a > (b > c)) > ((a > b) > (a > c))
    1. axiom:       a > (b > a)
    2. modus ponens:
            получено (A > B) > (A > A)
            из 1=[a > (b > a)], 2=[(a > (b > c)) > ((a > b) > (a > c))]
            подстановкой во 2 a: (A), b: (B), c: (A)
            1=[a > (b > a)], 2=[(A > (B > A)) > ((A > B) > (A > A))]
    3. axiom:       a > (b > a)
    4. modus ponens:
            получено A > A
            из 1=[a > (b > a)], 2=[(A > B) > (A > A)]
            подстановкой во 2 a: (A), b: (B > A)
            1=[a > (b > a)], 2=[(A > B) > (A > A)]
    ```

---
+++ app/syllologism_pretty_print.py
@@ -0,0 +1,40 @@
+from app.utils.syllogism_result import SyllogismResult
+
+
+def pretty(syllogism_list: list[SyllogismResult]) -> str:
+    result_strings = []
+
+    for index, syllogism in enumerate(syllogism_list):
+        match syllogism:
+            case SyllogismResult('axiom' as syllogism_name, _, _, output_term):
+                result_strings.append(f'{index}. {syllogism_name}:\t{output_term}')
+
+            case SyllogismResult(
+                syllogism_name, input_terms, substitutions, output_term
+            ):
+                input_terms_desc = ', '.join(
+                    f'{i + 1}=[{term.output_term}]'
+                    for i, term in enumerate(input_terms)
+                )
+                substitutions_desc = ' '.join(
+                    f"""{i + 1} """
+                    f"""{", ".join(f"{key}: ({sub_t})" for key, sub_t in subs.items())}"""
+                    for i, subs in enumerate(substitutions)
+                    if subs
+                )
+                substituted_terms_desc = ', '.join(
+                    f'{i + 1}=[{syll.output_term.substitute(**subs)}]'
+                    for i, (syll, subs) in enumerate(
+                        zip(input_terms, substitutions)
+                    )
+                )
+
+                result_strings.append(
+                    f"""{index}. {syllogism_name}:\n"""
+                    f"""\tполучено {output_term}\n"""
+                    f"""\tиз {input_terms_desc}\n"""
+                    f"""\tподстановкой во {substitutions_desc}\n"""
+                    f"""\t{substituted_terms_desc}"""
+                )
+
+    return '\n'.join(result_strings)
---
+++ tests/syllologism_pretty_print_test.py
@@ -0,0 +1,30 @@
+import pytest
+from app.terms import Arrow, Var, Not
+from app.bfs import bfs
+from app.syllologism_pretty_print import pretty
+
+A = Var('a')
+B = Var('b')
+C = Var('c')
+
+
+def test_pretty(axioms):
+    target = Arrow(Var('A'), Var('A'))
+    arrowed = target.to_implication_view().unify()
+    bfs_result = bfs(axioms, arrowed)
+    pretty_result = pretty(bfs_result)
+    print()
+    print(pretty_result)
+    assert pretty_result == ''
+
+
+@pytest.fixture()
+def axioms():
+    A1 = Arrow(A, Arrow(B, A))
+    A2 = Arrow(Arrow(A, Arrow(B, C)), Arrow(Arrow(A, B), Arrow(A, C)))
+    A3 = Arrow(Arrow(Not(B), Not(A)), Arrow(Arrow(Not(B), A), B))
+    B3 = Arrow(Arrow(A, B), Arrow(Arrow(A, Not(B)), Not(A)))
+    F4 = Arrow(Not(Not(A)), A)
+    TT = Arrow(Not(A), Arrow(A, B))
+    GA = Arrow(Arrow(Arrow(A, B), Arrow(B, C)), Arrow(A, C))
+    return [A1, A2, A3]
